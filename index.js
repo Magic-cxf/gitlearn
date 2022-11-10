5ff5ed5d05d08f620a3c5adfaa76993f67c79330
MagicSqe <MagicSqe0606@petalmail.com>
2022-11-10 19:44:25 +0800

    modify index

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ People.prototype.walk = function (){
 }
 
 
-const xiaoming = new People("小明",20)
+
 
 async function test(){
     console.log("start")
@@ -49,6 +49,71 @@ instance.interceptors.response.use((response)=>{
     return response
 })
 
-instance.get("http://www.baidu.com").then((res)=>{
-    console.log(res.status)
-})
+// instance.get("http://www.baidu.com").then((res)=>{
+//     console.log(res.status)
+// })
+
+// function content(obj){
+//     function F(){}
+//     F.prototype = obj
+//     return new F()
+// }
+
+// function Person(name){
+//     this.name = name
+// }
+
+// Person.prototype.subject = "math"
+
+// const xioahong = new Person("小红")
+
+// xioahong.school = "csuft"
+
+// for(let key in xioahong){
+//     console.log(key)
+// }
+// Object.keys(xioahong).forEach((item)=>{
+//     console.log(item)
+// })
+
+// const ls = new Map()
+
+// ls.set(xioahong,[1,2,4,5])
+
+
+// for ( let [item, index] of Object.entries(xioahong)){
+//     console.log(item,index)
+// }
+
+
+/*
+利用递归 实现  斐波那契数列
+*/ 
+function feibo(n){
+    if (n == 1 || n==2){
+        return 1
+    }else{
+        return feibo(n-1)+feibo(n-2)
+    }
+
+}
+
+
+/*
+利用动态规划实行斐波那契
+*/ 
+
+function feibonaci(n){
+    if(n<= 1){
+        return n
+    }
+    let current = 1,result = 0,pre = 0
+    for(let i=1;i<n;i++){
+        result = pre+current
+        pre = current
+        current = result
+    }
+    console.log(result)
+}
+
+feibonaci(45)
